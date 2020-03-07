07d1e30a75b38b7d1052dedfcfbce92f9ca7238f
kneeraj825 <kneeraj825.com>
2020-03-07 17:40:35 +0530

    Added:Calculating monthly salary of the employee

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,9 +4,6 @@ namespace Helloworld
 {
     class Program
     {
-
-
-
         public static void Main(string[] args)
         {
             //constants
@@ -14,23 +11,30 @@ namespace Helloworld
             int EMP_WAGE_PER_HRS = 20;
             int EMP_FULL_TIME = 8;
             int EMP_PART_TIME = 4;
+            int TOTAL_DAYS_IN_MONTH = 20;
+
+            int totalSalaryOfMonth = 0;
             Random ran = new Random();
             int val = ran.Next(0, 3);
-            //calculating the salary using case statement
+            //calculating the monthly salary 
             switch (val)
-            {
+            { 
                 case 1:
                     int FULL_DAY_SALARY = (EMP_WAGE_PER_HRS * EMP_FULL_TIME);
                     Console.WriteLine("total salary is " + FULL_DAY_SALARY);
+                    totalSalaryOfMonth = (FULL_DAY_SALARY * TOTAL_DAYS_IN_MONTH);
+                    Console.WriteLine("total salary of the month is " + totalSalaryOfMonth);
                     break;
                 case 2:
                     int PART_TIME_SALARY = (EMP_WAGE_PER_HRS * EMP_PART_TIME);
                     Console.WriteLine("total salary is " + PART_TIME_SALARY);
+                    totalSalaryOfMonth = (PART_TIME_SALARY * TOTAL_DAYS_IN_MONTH);
+                    Console.WriteLine("total salary of the month is " + totalSalaryOfMonth);
                     break;
-
                 default:
                     Console.WriteLine("employee is absent");
                     Console.WriteLine("total salary is " + IS_ABSENT);
+                    Console.WriteLine("total salary of the month is " + IS_ABSENT);
                     break;
             }
         }
