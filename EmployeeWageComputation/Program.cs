81594740066f91968035f074223ac51ce9cab09a
kneeraj825 <kneeraj825.com>
2020-03-07 17:27:50 +0530

    Added:Calculating the salary using caes statement

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,39 +1,38 @@
 ﻿using System;
 
-namespace EmployeeWageComputation
-{        
+namespace Helloworld
+{
     class Program
     {
-        static void Main(string[] args)
+
+
+
+        public static void Main(string[] args)
         {
-            //constatns
-            int IS_PRESENT = 1;
-            int EMP_ABSENT = 0;
-            int IS_PART_TIME = 2;
+            //constants
+            int IS_ABSENT = 0;
+            int EMP_WAGE_PER_HRS = 20;
             int EMP_FULL_TIME = 8;
             int EMP_PART_TIME = 4;
-            int EMP_WAGE_PER_HRS = 20;
-
             Random ran = new Random();
             int val = ran.Next(0, 3);
-            //checking the salary of employee
-            if (val == IS_PRESENT)
+            //calculating the salary using case statement
+            switch (val)
             {
-                Console.WriteLine("employee is present");
-                int FULL_DAY_SALARY = (EMP_WAGE_PER_HRS * EMP_FULL_TIME);
-                Console.WriteLine("total salary is " + FULL_DAY_SALARY);
+                case 1:
+                    int FULL_DAY_SALARY = (EMP_WAGE_PER_HRS * EMP_FULL_TIME);
+                    Console.WriteLine("total salary is " + FULL_DAY_SALARY);
+                    break;
+                case 2:
+                    int PART_TIME_SALARY = (EMP_WAGE_PER_HRS * EMP_PART_TIME);
+                    Console.WriteLine("total salary is " + PART_TIME_SALARY);
+                    break;
+
+                default:
+                    Console.WriteLine("employee is absent");
+                    Console.WriteLine("total salary is " + IS_ABSENT);
+                    break;
             }
-            else if (val == IS_PART_TIME)
-            {
-                int PART_TIME_SALARY = (EMP_WAGE_PER_HRS * EMP_PART_TIME);
-                Console.WriteLine("total salary is " + PART_TIME_SALARY);
-            }
-            else
-            {
-                Console.WriteLine("employee is absent");
-                Console.WriteLine("total salary is " + EMP_ABSENT);
-            }
-            Console.WriteLine(val);
         }
     }
 }
